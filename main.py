78fdfda3e085c5b00b6581bbf865d8f932a6c0a4
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-05 12:20:07 -0500

    add other element of form in CafeForm in main.py and import render_form() to render the form passed to add.html

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
 import csv
 
 '''
@@ -25,12 +25,19 @@ Bootstrap5(app)
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired(), URL()])
+    open_time = StringField('Open', validators=[DataRequired()])
+    close_time = StringField('closed', validators=[DataRequired()])
+    coffe_rating = SelectField('Coffee', choices=["☕", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
+    wifi_rating = SelectField('Wifi', choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
+    power_rating = SelectField('Power', choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 # Exercise:
 # add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
 # make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
+# e.g. You could use emojis ☕️/💪/✘/🔌
 # make all fields required except submit
 # use a validator to check that the URL field has a URL entered.
 # ---------------------------------------------------------------------------
@@ -42,11 +49,26 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
         print("True")
+        with open('cafe-data.csv', mode='a', encoding='utf-8') as csv_file:
+            row = [form.cafe.data, form.location.data, form.open_time.data, form.close_time.data,
+                   form.coffe_rating.data, form.wifi_rating.data, form.power_rating.data]
+            for index in range(0, len(row)):
+                if index == 0:
+                    csv_file.write(f"\n{row[index]}")
+                    csv_file.write(", ")
+                elif index == len(row) - 1:
+                    csv_file.write(row[index])
+                else:
+                    csv_file.write(row[index])
+                    csv_file.write(", ")
+
+
+
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
